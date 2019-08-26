3011d6da22648f077fa7019cb00717ec99f1a4b1
FaysalAlaoui <faysal.alaoui.inbox@gmail.com>
2019-08-26 19:33:46 +0100

    preparing to deploy on gh-pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyDeQHaU8bsy0yPlz9dwqiyq_GbS749PfAQ",
+    authDomain: "oshop-00001.firebaseapp.com",
+    databaseURL: "https://oshop-00001.firebaseio.com",
+    projectId: "oshop-00001",
+    storageBucket: "",
+    messagingSenderId: "72963511994",
+    appId: "1:72963511994:web:c3cdfa03110f1273"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyDeQHaU8bsy0yPlz9dwqiyq_GbS749PfAQ",
+    authDomain: "oshop-00001.firebaseapp.com",
+    databaseURL: "https://oshop-00001.firebaseio.com",
+    projectId: "oshop-00001",
+    storageBucket: "",
+    messagingSenderId: "72963511994",
+    appId: "1:72963511994:web:c3cdfa03110f1273"
+  }
 };
 
 /*
